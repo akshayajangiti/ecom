61e73a6601bf1ebab1349e5b7869faf472ed5e52
akshayajangiti <akshayajangiti1104.com>
2023-09-12 15:59:53 +0530

    Successfull

---
+++ app_server/models/db.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const dbURI = 'mongodb://localhost:27017';
+mongoose.connect(dbURI, {useNewUrlParser: true});
+
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${dbURI}`);
+    });
+
+mongoose.connection.on('error', err => {
+    console.log('Mongoose connection error:', err);
+    });
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+    });
+
+
+    const readLine = require ('readline');
+    if (process.platform === 'win32'){
+    const rl = readLine.createInterface ({
+    input: process.stdin,
+    output: process.stdout
+    });
+    rl.on ('SIGINT', () => {
+    process.emit ("SIGINT");
+    });
+    }
